c519580e27e0c694f9a731279b543ec1d853820d
Ivancho894 <65725085+Ivancho894@users.noreply.github.com>
2023-03-30 10:24:23 -0300

    Add files via upload

---
+++ tarea1/FirstClass.java
@@ -0,0 +1,18 @@
+package tarea1;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        int number1 = 1;
+        double number2 = 2;
+        boolean number3 = true;
+        String number4 = "Que miras bobo";
+        long number5 =32423;
+
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+        System.out.println(number4);
+        System.out.println(number5);
+
+    }
+}
