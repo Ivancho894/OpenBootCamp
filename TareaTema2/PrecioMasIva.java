64e545fa8ccb4052b6264cee7e194eb37f219760
Ivancho894 <65725085+Ivancho894@users.noreply.github.com>
2023-03-30 10:43:34 -0300

    Update PrecioMasIva.java

--- TareaTema2/PrecioMasIva.java
+++ TareaTema2/PrecioMasIva.java
@@ -1,17 +1,19 @@
 package TareaTema2;
-
+import java.util.Scanner;
 public class PrecioMasIva {
 
     public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
 
+        System.out.println("Introduce el numero para calcular iva: ");
+        int preci = scanner.nextInt();
+        System.out.println("Cual es el IVA actual en tu pais: ");
+        int iva = scanner.nextInt();
 
-        int preci = 300;
-
-        System.out.println(ivi(preci));
+        System.out.println(ivi(preci,iva));
 
     }
-    public static int ivi(int prec) {
-        return prec+(21*prec/100);
+    public static int ivi(int prec,int iva) {
+        return prec+(iva*prec/100);
     }
-
 }
