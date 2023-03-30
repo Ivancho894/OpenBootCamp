a1e19327114d5d34e6ae34ec9bdbb046b13f3bfd
Ivancho894 <65725085+Ivancho894@users.noreply.github.com>
2023-03-30 16:13:45 -0300

    Add files via upload

---
+++ EjercicioTema3/Tema3.java
@@ -0,0 +1,14 @@
+package EjercicioTema3;
+
+public class Tema3 {
+    public static void main(String[] args) {
+
+
+        String[] nombres = {"Nombre", "Nombre2", "Nombre3", "Nombre4", "Nombre5"};
+
+        for (String nombre : nombres) {
+            System.out.println(nombre);
+        }
+    }
+
+}
